6e30b3f11ec1759c8791e9d514c3c5aef8af5c9f
baparicio <baparicio.sc@gmail.com>
2020-12-09 17:13:23 +0100

    SQL Acabado

--- src/JDBC/JDBC2_Bernat__Aparicio.java
+++ src/JDBC/JDBC2_Bernat__Aparicio.java
@@ -9,45 +9,141 @@ public class JDBC2_Bernat__Aparicio {
     private static Connection conn = null;
     private static Statement stmt;
     private static ResultSet rs = null;
-    private static String url = "jdbc:mysql://localhost:3306/demo?serverTimezone="+ TimeZone.getDefault().getID();
+    private static String url = "jdbc:mysql://localhost:3306/demo?serverTimezone=" + TimeZone.getDefault().getID();
     private static String user = "root";
     private static String password = "Bernat1234";
     private static String driver = "com.mysql.cj.jdbc.Driver";
 
-    public static void forNameDriver(){
+    public static void forNameDriver() {
         // Métodos
-        try{
+        try {
             Class.forName(driver);
-        }
-        catch(ClassNotFoundException e){
+        } catch (ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    public static void actDepPrecedure(int numDept, String nomDept){
+    public static void actDepPrecedure(int numDept, String nomDept) {
         // Trabajo con base de datos
-        try{
+        try {
             conn = DriverManager.getConnection(url, user, password);
             stmt = conn.createStatement();
-            stmt.executeUpdate("call actualizaDept("+ numDept+ ", '"+ nomDept+"');");
+            stmt.executeUpdate("call actualizaDept(" + numDept + ", '" + nomDept + "');");
             stmt.close();
-        }
-        catch(SQLException e){
+        } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally{
-            if(conn != null){
-                try{
+        } finally {
+            if (conn != null) {
+                try {
                     conn.close();
-                }
-                catch(SQLException e){
+                } catch (SQLException e) {
                     e.printStackTrace();
                 }
             }
         }
     }
 
+    public static Object getObjectProcedure(int numDept) {
+        String dname = "";
+        String loc = "";
+        // Trabajo con base de datos
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            CallableStatement cst =conn.prepareCall("call consultaDepar("+numDept+", ?, ?);");
+            cst.registerOutParameter(1, java.sql.Types.VARCHAR);
+            cst.registerOutParameter(2, java.sql.Types.VARCHAR);
+            cst.execute();
+            dname = cst.getString(1);
+            loc = cst.getString(2);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+                }
+            }
+        }
+        departament dept = new departament(numDept, dname, loc);
+        return dept;
+    }
+
+    public static void actSAL(int deptno, int aumento) {
+        // Trabajo con base de datos
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            stmt = conn.createStatement();
+            stmt.executeUpdate("call actualizarSueldo(" + deptno + ", '" + aumento + "');");
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void actSALRS(int deptno, int aumento) {
+        // Trabajo con base de datos
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            stmt = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = stmt.executeQuery("select * from emp where DEPTNO = "+deptno+";");
+            while (rs.next()){
+                double salarioNuevo = rs.getDouble("SAL") + aumento;
+                rs.updateDouble("SAL",salarioNuevo);
+                rs.updateRow();
+            }
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void infoBD() {
+        // Trabajo con base de datos
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            DatabaseMetaData infoBD = conn.getMetaData();
+            String user = infoBD.getUserName();
+            String driver = infoBD.getDriverName();
+            String gestor = infoBD.getDatabaseProductName();
+            System.out.println(user + driver + gestor);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) {
-        actDepPrecedure(10,"SPAIN");
+        actDepPrecedure(10, "SPAIN");
+        getObjectProcedure(10);
+        //actSAL(10, 100);
+        actSALRS(10, 100);
+        infoBD();
     }
 }
